e2a2b386e2b8bf2b4c97232beb5740e5b5a6a742
quentintrouve <trouve.quentin@gmail.com>
2022-01-25 14:48:51 +0100

    add new post route

--- src/Controller/RouterController.php
+++ src/Controller/RouterController.php
@@ -2,10 +2,17 @@
 
 namespace App\Controller;
 
+
+use App\Entity\User;
+use App\Entity\Category;
+use App\Entity\Post;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use DateTime;
 
 class RouterController extends AbstractController
 {
@@ -17,5 +24,43 @@ class RouterController extends AbstractController
     {
         $posts = $postRepository->findAllPostsOrderedByNewest();
         return $this->render('Pages/home.html.twig', ["posts" => $posts]);
+    }
+
+    /**
+     * @Route("post/new", name="app_newPost")
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    public function newPost(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        #$author = $request->request->get('author');
+        #$category = $request->request->get('category');
+        #$condition = $request->request->get('condition');
+        #$price = $request->request->get('price');
+        #$description = $request->request->get('description');
+        #$image = $request->request->get('image');
+
+
+        $user = $entityManager->getReference(User::class, 8);
+        $category = $entityManager->getReference(Category::class, 1);
+        $created_at = new DateTime();
+
+
+        $post = (new Post())
+            ->setAuthor($user)
+            ->setCategory($category)
+            ->setCondition("")
+            ->setPrice(4)
+            ->setTitle("Poster de batman")
+            ->setCreatedAt($created_at)
+            ->setDescription("Poster de batman 60x130. Très bon état.")
+            ->setIsPublished(true)
+            ->setImages([]);
+
+        $entityManager->persist($post);
+        $entityManager->flush();
+
+        return new Response(sprintf('nouvelle annonce avec le titre : %s', $post->getTitle()));
     }
 }
