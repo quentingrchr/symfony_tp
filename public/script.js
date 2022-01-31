783232cc9e98a0077846f103a6aeb9972d74056c
quentingrchr <51860690+quentingrchr@users.noreply.github.com>
2022-01-31 17:36:25 +0100

    add useless-form

--- public/script.js
+++ public/script.js
@@ -3,6 +3,7 @@ const $forms = document.querySelectorAll('form');
 $forms.forEach( $form =>{
     const $resetBtn = $form.querySelector('[data-reset-form-value]');
     const $resetInputs = $form.querySelectorAll('[data-old-value]');
+    if(!$resetBtn) return
 
     $resetBtn.addEventListener('click',(e)=>{
         e.preventDefault();
@@ -12,4 +13,36 @@ $forms.forEach( $form =>{
     })
 })
 
+/* Profile page filter
+ */
+const $profilePostsContainer = document.querySelector("#your-post")
+const $profileForms = document.querySelectorAll(".form-check-input")
+let displayState = {
+    'draft': true,
+    'published': true
+}
+$profileForms.forEach(($form)=> {
+    $form.addEventListener('change', (e) => {
+        if(e.target.value){
+            displayState[e.target.value] = !displayState[e.target.value]
+        }
+        updateFilter(displayState)
+    })
+})
+function updateFilter(state){
+  const $posts =  $profilePostsContainer.querySelectorAll("[data-draft]")
+    $posts.forEach($post => {
+
+        const isDraft = $post.getAttribute('data-draft')
+
+        if(isDraft === "true") {
+            console.log($post, 'draft')
+            state.draft ? $post.classList.remove('hidden') :  $post.classList.add('hidden');
+
+        } else {
+            // isPublished
+            state.published ? $post.classList.remove('hidden') :  $post.classList.add('hidden');
+        }
+    })
+}
 
